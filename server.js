55e0397debe004fe2a9602da2c7d33c183761dea
minfromdavis <minchin903@gmail.com>
2024-03-02 13:34:07 -0800

    changes to server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
