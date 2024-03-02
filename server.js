0233678df2de832a91b63bbeb4f7a99a7848a7ab
minfromdavis <minchin903@gmail.com>
2024-03-02 13:17:33 -0800

    changed port to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80;
+const port = 3000;
 const bodyParser = require('body-parser');
 var cors = require('cors')
 app.use(bodyParser.json());
