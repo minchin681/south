bd50e575c2792d3304e77a5d8d1ae2fb214b9599
minfromdavis <minchin903@gmail.com>
2024-03-02 01:24:28 -0800

    add server code

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser');
+var cors = require('cors')
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
