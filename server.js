ce6889d25927fe434b8e1da7a35386a0f995d0b9
minfromdavis <minchin903@gmail.com>
2024-03-02 13:29:11 -0800

    remove cors

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080;
 const bodyParser = require('body-parser');
-var cors = require('cors')
 app.use(bodyParser.json());
-app.use(cors());
 
 
 app.listen(port, () => {
