e2165fb03b76157db6e3bdfaf32660e86d41c89f
minfromdavis <minchin903@gmail.com>
2024-03-02 12:40:48 -0800

    added port 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 80;
+const port = 8080;
 const bodyParser = require('body-parser');
 var cors = require('cors')
 app.use(bodyParser.json());
