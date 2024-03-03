5b700a75e3da4d6038c9ec0e64ba59a6abb2de2d
minfromdavis <minchin903@gmail.com>
2024-03-02 18:07:05 -0800

    added build

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const app = express()
 const port = 8080;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const path = require('path')
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static("./client/build"))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
