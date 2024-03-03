e5537e87074f40c46a9ef83ee41aeac5aa155d76
minfromdavis <minchin903@gmail.com>
2024-03-03 15:12:31 -0800

    change url again

--- client/src/components/dropoff.js
+++ client/src/components/dropoff.js
@@ -16,7 +16,7 @@ function Dropoff() {
   
     const fetchData = async () => {
       try {
-          const response = await fetch('wash-house.azurewebsites.net');
+          const response = await fetch('https://wash-house.azurewebsites.net');
           const result = await response.json();
           setApp(result);
       } catch (error) {
